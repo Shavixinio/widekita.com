987f0ffb0c12844d1d1e4483ff1d0a57b3cf2351
Shavixinio <mrmater121@gmail.com>
2026-01-19 17:09:15 +0100

    Initial commit for the Vite + Vue rewrite

---
+++ src/components/Clicker.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+  <div class="card">
+    <button type="button" @click="count++">
+      <img src="../assets/widekita.jpeg" alt="My wife that is very WIDE (my REAL wife is Ryo Yamada, sorry guys)">
+      {{ count }}</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
